54352ddf4b37325c6c537586258f6e5b0a8d0466
subasthican <manoharansubasthican@gmail.com>
2026-02-19 09:08:52 +0530

    Add nest-backend folder

---
+++ nest-backend/src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UsersService {
+  private users = [];
+
+  // CREATE
+  createUser(user: any) {
+    this.users.push(user);
+    return { message: 'User added successfully' };
+  }
+
+  // READ
+  getUsers() {
+    return this.users;
+  }
+
+  // UPDATE
+  updateUser(index: number, updatedUser: any) {
+    this.users[index] = updatedUser;
+    return { message: 'User updated successfully' };
+  }
+
+  // DELETE
+  deleteUser(index: number) {
+    this.users.splice(index, 1);
+    return { message: 'User deleted successfully' };
+  }
+}
